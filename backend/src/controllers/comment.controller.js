049d347ef6084b1b146fd016b7345620871c5207
Amir Grev <amirgrev14@gmail.com>
2026-05-01 19:30:33 +0300

    added comment routews and them controller

---
+++ backend/src/controllers/comment.controller.js
@@ -0,0 +1,95 @@
+// packages
+import { getAuth } from "@clerk/express";
+import  { asyncHandler } from "../utils/asyncHandler.js";
+
+// models
+import Comment from "../modles/comment.model.js";
+import User from "../modles/user.model.js";
+import Post from "../modles/post.model.js";
+import Notification from "../modles/notification.model.js"; 
+
+export const getComments = asyncHandler(async (req, res) => {
+  const { postId } = req.params;
+
+  const comments = await Comment.find({ post: postId })
+    .sort({ createdAt: -1 })
+    .populate("user", "username firstName lastName profilePicture")
+
+  res.status(200).json({ comments });
+});
+
+export const createComment = asyncHandler(async (req, res) => {
+  const { userId } = getAuth(req);
+  const { postId } = req.params;
+  const { content } = req.body;
+
+    if (!content || content.trim() === "") {
+      return res.status(400).json({ error: "Comment content is required" });
+    }
+
+  const user = await User.findOne({ clerkId: userId });
+  const post = await Post.findById(postId);
+
+    if(!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+  if(!post) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  const comment =  await Comment.create({
+    user: user._id,
+    post: post._id,
+    content,
+  });
+
+  // link the comment to the post
+  await Post.findByIdAndUpdate(postId, {
+    $push: { comments: comment._id },
+  });
+
+  // create notification if not commenting on won post
+  if (post.user.toString() !== user._id.toString()) {
+    await Notification.create({
+      from: user._id,
+      to: post.user,
+      type: "comment",
+      post: postId,
+      comment: comment._id,
+    });
+  }
+
+  res.status(201).json({ comment})
+})
+
+export const deleteComment = asyncHandler(async (req, res) => {
+  const { userId } = getAuth(req);
+  const { commentId } = req.params;
+
+  const user = await User.findOne({ clerkId: userId })
+  const comment = await Comment.findById(commentId);
+
+  if(!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  if(!comment) {
+    return res.status(404).json({ error: "Comment not found" });
+  }
+
+  if(comment.user.toString() !== user._id.toString()) {
+    return res.status(403).json({ error: "You can only delete your own comments" });
+  }
+
+  // remove comment from post 
+  await Post.findByIdAndUpdate(comment.post, {
+    $pull: { comments: commentId },
+  });
+
+  // delete the comment
+  await Comment.findByIdAndDelete(commentId)
+
+  res.status(200).json({ message: "Comment deleted successfully" })
+
+})
