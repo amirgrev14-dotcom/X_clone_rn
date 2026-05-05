9c538cf95a0f4050e4dbab3b0e339b3610eac333
Amir Grev <amirgrev14@gmail.com>
2026-05-05 14:32:41 +0300

    added reject

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { clerkMiddleware } from "@clerk/express";
 
 // routes
 import userRoutes from "./routes/user.route.js"
@@ -8,13 +7,20 @@ import postRoutes from "./routes/post.route.js"
 import commentRoutes from "./routes/comment.route.js"
 import notificationRoutes from "./routes/notification.route.js"
 
+import { clerkMiddleware } from "@clerk/express";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
+
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js"
 
 const app = express()
+// apply middlewares
 app.use(cors())
 app.use(express.json())
+
+// apply Arcjet middleware before all routes for security and rate limiting
 app.use(clerkMiddleware())
+app.use(arcjetMiddleware())
 
 // health check
 app.get("/api/health", (req, res) => {
@@ -28,11 +34,17 @@ app.use("/api/comments", commentRoutes)
 app.use("/api/notifications", notificationRoutes)
 
 const startServer = async () => {
-  await connectDB();
+  try {
+    await connectDB();
 
-  app.listen(ENV.PORT, () => {
-    console.log(`Server running on port ${ENV.PORT}`)
-  });
+    // listen for local development
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+    }
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
 };
 
 startServer()
